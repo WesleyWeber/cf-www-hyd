bed6943adc7b6b6cb64e1cb442c43a404c244b64
WW <a@b.c>
2026-01-09 11:32:24 -0500

    updating colors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request): Promise<Response> {
 		const html = `<!DOCTYPE html>
-		<body>
+		<body style="background-color: lightgreen; color: black;">
 		  <h1>How you doin?</h1>
 		  <p>F U Z Z Y L O G I C</p>
 		</body>`;
