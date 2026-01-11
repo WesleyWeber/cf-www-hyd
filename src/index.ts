8b929a5a98cfada8db3803f91f93e2964b73b3ed
WW <a@b.c>
2026-01-10 23:23:15 -0500

    stylin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 export default {
 	async fetch(request): Promise<Response> {
 		const html = `<!DOCTYPE html>
+
+		<style>
+			.center-text {
+				width: 50%; /* Optional: define a width */
+				margin-left: auto;
+				margin-right: auto;
+			}
+		</style>
+
 		<body style="background-color: lightgreen; color: black;">
-		  <h1>How you doin?</h1>
-		  <p>F U Z Z Y L O G I C</p>
+			<div class="center-text">  
+				<h1>How you doin?</h1>
+			</div>
 		</body>`;
 
 		return new Response(html, {
